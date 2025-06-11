1aba53809aff8502cb9b899ed561cc5118be7795
masaya <batbayar.tsogbayar.rokda@gmail.com>
2025-06-11 19:41:07 +0800

    Added post route in violation

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const rateLimiterMiddleware = require("./middlewares/rateLimiter");
-const logger = require("./middlewares/logger"); 
+const logger = require("./middlewares/logger");
 const authMiddleware = require("./middlewares/authMiddleware");
 const cors = require('cors');
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3890',
-        'http://103.9.90.140:3880'
+        'http://160.22.211.141:3880'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true // Allow cookies to be sent along with requests if necessary
@@ -21,12 +21,13 @@ app.use(cors({
 app.use(logger);
 app.use(rateLimiterMiddleware);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Unprotected routes
 app.use("/api", require("./routes/auth"));
 app.use("/api/v1/violation", require("./routes/violation"));
+app.use("/api/v1/violationb", require("./routes/violoationBodi"));
 
 
 
